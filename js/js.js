ff3c10b5ebd5ac1717f6dc7ade815b4d336acee1
Son Pham <spham7387@gmail.com>
2025-06-28 20:16:57 +0700

    My_portfolio_flie

---
+++ js/js.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navbarHTML = `
+    <nav class="navbar">
+      <button class="menu-toggle">☰</button>
+      <ul>
+        <li><a href="home.html">HOME</a></li>
+        <li><a href="about.html">ABOUT</a></li>
+        <li><a href="contact.html">CONTACT</a></li>
+      </ul>
+    </nav>
+  `;
+
+  // Chèn navbar vào thẻ có id="navbar"
+  document.getElementById("navbar").innerHTML = navbarHTML;
+
+  // Thêm xử lý toggle menu
+  const toggle = document.querySelector(".menu-toggle");
+  const navList = document.querySelector(".navbar ul");
+
+  toggle.addEventListener("click", function () {
+    navList.classList.toggle("active");
+  });
+});
