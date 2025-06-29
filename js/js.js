daa1a62ffdc73fa9ae0d75d8d4cf06b23cf9eaa3
Son Pham <spham7387@gmail.com>
2025-06-29 09:53:17 +0700

    Update js.js

--- js/js.js
+++ js/js.js
@@ -3,17 +3,24 @@ document.addEventListener("DOMContentLoaded", function () {
     <nav class="navbar">
       <button class="menu-toggle">☰</button>
       <ul>
-        <li><a href="index.html">HOME</a></li>
+        <li><a href="home.html">HOME</a></li>
         <li><a href="about.html">ABOUT</a></li>
         <li><a href="contact.html">CONTACT</a></li>
       </ul>
     </nav>
   `;
 
-  // Chèn navbar vào thẻ có id="navbar"
   document.getElementById("navbar").innerHTML = navbarHTML;
 
-  // Thêm xử lý toggle menu
+  // Gán class active theo URL
+  const currentPage = location.pathname.split("/").pop();
+  document.querySelectorAll(".navbar ul li a").forEach(link => {
+    if (link.getAttribute("href") === currentPage) {
+      link.classList.add("active");
+    }
+  });
+
+  // Toggle menu cho mobile
   const toggle = document.querySelector(".menu-toggle");
   const navList = document.querySelector(".navbar ul");
 
